2a578b84d5fb8bdd3bd0711af0939611679c410f
Ernesto Bonet Moncada <bernestoalberto@gmail.com>
2019-03-15 00:55:14 -0400

    update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -10,8 +10,10 @@ module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
-            port: 8545,
-            network_id: "*" // Match any network id
+            port: 7545,
+            network_id: "*", // Match any network id
+            gas: 4712388,
+            gasPrice: 100000000000
         },
         rinkeby: {
             provider: function() {
